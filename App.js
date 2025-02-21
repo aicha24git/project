8fe9817a587cf6bb379838142cc3e47d4937adc4
aicha24git <aichaibrahim2004@gmail.com>
2025-02-21 12:52:43 +0100

    Ajout d'un icône approprié

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import AddNoteScreen from "./src/components/NoteForm";
+import NoteDetailScreen from "./src/screens/NoteDetailScreen";
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#4CAF50",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Notes & Tasks" }}
+        />
+        <Stack.Screen
+          name="AddNote"
+          component={AddNoteScreen}
+          options={{ title: "Nouvelle Note" }}
+        />
+        <Stack.Screen
+          name="NoteDetail"
+          component={NoteDetailScreen}
+          options={{ title: "Détails" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
